35c1c48da6010907b396c7034123fe7a996fe2ad
ranchosevens <dyzhuo@zju.edu.cn>
2019-05-19 14:32:56 +0800

    a simple vgg net

--- examples/simple_vgg.py
+++ examples/simple_vgg.py
@@ -9,12 +9,12 @@ from keras.utils import np_utils
 
 # Generate dummy data
 x_train = np.random.random((100, 100, 100, 3))
-# 100张图片，每张100*100*3
+# 100张图片，每张100 * 100 * 3
 y_train = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
-# 100*10
+# 100 * 10
 x_test = np.random.random((20, 100, 100, 3))
 y_test = keras.utils.to_categorical(np.random.randint(10, size=(20, 1)), num_classes=10)
-# 20*100
+# 20 * 100
 
 model = Sequential()
 # input:100*100 images with 3 channels -> (100,100,3) tensors
@@ -26,16 +26,16 @@ model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(256,activation='relu'))
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(10,activation='softmax'))
+model.add(Dense(10, activation='softmax'))
 
-sgd=SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
-model.compile(loss='categorical_crossentropy',optimizer=sgd)
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
-model.fit(x_train,y_train,batch_size=32,epochs=10)
-score=model.evaluate(x_test,y_test,batch_size=32)
+model.fit(x_train, y_train, batch_size=32, epochs=10)
+score = model.evaluate(x_test, y_test, batch_size=32)
